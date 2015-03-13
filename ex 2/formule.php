7a0e7fdad68fbe8113fd6ea7286ba640bbef7f4f
Bastien Nicoud <bastien.nicoud@cpnv.ch>
2015-03-13 12:39:57 +0100

    ajout exercice 2

---
+++ ex 2/formule.php
@@ -0,0 +1,9 @@
+<?php
+    $Login    = $_POST['Login'];
+    $MotDePasse = $_POST['Mot de passe'];
+    $majeur    = $_POST['majeur'];
+    $type    = $_POST['type'];
+    $options    = $_POST['options'];    
+    
+    echo "$Login $MotDePasse $majeur $type $options"
+?>
